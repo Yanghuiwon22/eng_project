5833ccb903a99cacc45cb5c38223219a2838d72f
Yanghuiwon22 <hannah4212@naver.com>
2023-11-20 13:23:08 +0900

    archive setting -ing

--- archive/urls.py
+++ archive/urls.py
@@ -4,8 +4,7 @@ from . import  views
 urlpatterns = [
     # path('<int:pk>/',views.single_post_page),
     # path('', views.index),
-    # path('<int:pk>/', views.PostDetail.as_view()),
-    # path('', views.PostList.as_view())
-    path('', view.DataList.as_view()),
-    path('<int:pk>/', views.DataDetail.as_view())
+    path('<int:pk>/', views.DataDetail.as_view()),
+    path('', views.DataList.as_view()),
+
 ]
