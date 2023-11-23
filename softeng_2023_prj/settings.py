9957fed5f4d0a465bbda35e1e79bce297b09585a
Yanghuiwon22 <hannah4212@naver.com>
2023-11-23 17:22:28 +0900

    11. 23. 1차

--- softeng_2023_prj/settings.py
+++ softeng_2023_prj/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'notice',
     'bookstore',
     'reserve_meal',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -125,11 +126,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    # 정적 파일이 위치한 경로 추가
-    os.path.join(BASE_DIR, 'static'),
-]
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
