d5ca1c5a22c5d246fe513d681f6f58e8b9cfc4ae
Yanghuiwon22 <hannah4212@naver.com>
2023-11-27 10:10:58 +0900

    .

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,7 +3,8 @@ from . import  views
 
 urlpatterns = [
 
-    path('<int:pk>/', views.BookStoreDetail.as_view()),
-    path('', views.BookStoreList.as_view(), name='to_book_reg'),
+    # path('<int:pk>/', views.BookStoreDetail.as_view()),
+    path('', views.BookStoreList.as_view()),
     path('bookstore_datail/', views.BookStoreDetail.as_view(), name='to_book_reg'),
+
 ]
