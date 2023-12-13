f3a916c26f8a530a55ae18e942a2acaa9f278ee0
Yanghuiwon22 <hannah4212@naver.com>
2023-12-13 21:24:56 +0900

    reserve_meal. timetable 저장

--- reserve_meal/models.py
+++ reserve_meal/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+
 
 def reserve_upload_to(instance,filename):
-    sender_username = instance.sender.username if instance.sender else 'unknown_sender'
-    return f'reserve_meal/images/{instance.sender_username}/{filename}/'
+    instance.sender_username = instance.sender.username
+    extension = filename.split('.')[-1]  # 파일 확장자 추출
+    new_filename = f'{uuid.uuid4().hex}.{extension}'
+
+    # return f'reserve_meal/images/%Y/%M/%D/'
+    return f'reserve_meal/images/{instance.sender_username}/{new_filename}'
 
 # Create your models here.
 class ReserveMeal(models.Model):
@@ -11,5 +17,7 @@ class ReserveMeal(models.Model):
     receiver = models.ForeignKey(User, related_name='received_reserve', on_delete=models.CASCADE)
 
     timetable = models.ImageField(upload_to=reserve_upload_to)
+    def get_absolute_url(self):        # 폼을 성공적으로 처리 시 이동할 페이지 주소
+        return f'/reserve_meal/'
 
 
--- reserve_meal/views.py
+++ reserve_meal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import ReserveMeal
 from itertools import chain
@@ -8,18 +8,17 @@ from django.views.generic import CreateView
 def reserve_main(request):
     all_users = User.objects.exclude(pk=request.user.pk)
 
-    current_user = request.user.id
-
     return render(request, 'reserve_meal/reserve_main.html',
                   {'all_users': all_users})
 
 class ReserveMeal_Form(CreateView):
     model = ReserveMeal
-    fields = ['timetable']
+    fields = ['receiver','timetable']
 
-    def form_vaild(self, form):
+    def form_valid(self, form):
         current_user = self.request.user
         if current_user.is_authenticated:
             form.instance.sender = current_user
-            return super(ReserveMeal_Form, self).form_vaild(form)
+            return super(ReserveMeal_Form, self).form_valid(form)
+
 
