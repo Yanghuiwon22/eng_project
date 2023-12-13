16d36d3bf3b72a4058da60e6bf1858b0e6829b71
mj4247 <minjai4247@naver.com>
2023-12-13 21:50:29 +0900

    12/13 아카이브 모델스 뷰스 urls
    dd

--- archive/admin.py
+++ archive/admin.py
@@ -1,4 +1,12 @@
+# from django.contrib import admin
+# from .models import Archive
+#
+# admin.site.register(Archive)
+
 from django.contrib import admin
-from .models import Data
-# Register your models here.
-admin.site.register(Data)
+from .models import Archive
+
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display = ('title', 'professor', 'subject', 'student', 'created_at', 'category', 'writer')
+
+admin.site.register(Archive, ArchiveAdmin)
--- archive/urls.py
+++ archive/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path, include
-from . import  views
+# from django.urls import path, include
+# from . import  views
+#
+# urlpatterns = [
+#     # path('<int:pk>/',views.single_post_page),
+#     # path('', views.index),
+#     path('<int:pk>/', views.DataDetail.as_view()),
+#     path('', views.DataList.as_view()),
+#
+# ]
+
+
+from django.urls import path
+from .views import ArchiveList, ArchiveDetail, ArchiveForm_Form, archiveform_view, category_page
+
+app_name = 'archive'
 
 urlpatterns = [
-    # path('<int:pk>/',views.single_post_page),
-    # path('', views.index),
-    path('<int:pk>/', views.DataDetail.as_view()),
-    path('', views.DataList.as_view()),
-
+    path('<int:pk>/', ArchiveDetail.as_view(), name='archive_detail'),
+    path('archive_list/', ArchiveList.as_view(), name='archive_list'),
+    path('archive_form/', ArchiveForm_Form.as_view(), name='archive_form'),
+    path('archiveform_view/', archiveform_view, name='archiveform_view'),
+    path('category/<slug:slug>/', category_page, name='category_page'),
 ]
